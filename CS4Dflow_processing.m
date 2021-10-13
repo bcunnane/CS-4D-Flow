6298411d94d67dda6c7bfec3eb6b9ad6aad6bcd3
Brandon Cunnane <brandon.cunnane@gmail.com>
2021-10-13 13:51:43 -0700

    split phase & mag importing, added comments

--- CS4Dflow_processing.m
+++ CS4Dflow_processing.m
@@ -1,45 +1,51 @@
 %% CS4Dflow processing
-% description
+% Script for processing DICOMS collected from Siemens CS 4D flow sequence
 
-%% import vepc data
+%% Import vepc data
 % go to desired vepc data directory
 dcm_files = dir('*IMA');
 
-for k = length(dcm_files):-1:1
+% import velocity phase data
+for k = 24:-1:1%length(dcm_files):-1:1
     info = dicominfo(dcm_files(k).name);
-    vepc_data(:,:,k) = dicomread(info);
-    %vepc_data(:,:,k) = int16(dicomread(info))*info.RescaleSlope + info.RescaleIntercept;
+    vepc_data(:,:,k) = int16(dicomread(info))*info.RescaleSlope + info.RescaleIntercept;
     locs(k,1) = info.SliceLocation;
 end
 
-try
-    direction = info.Private_0051_1014;
-    if contains(direction,'through')
-        dynamic.vz = vepc_data;
-    elseif contains(direction,'ap')
-        dynamic.vx = vepc_data;
-    elseif contains(direction,'fh')
-        dynamic.vy = vepc_data;
-    end
-catch
-    dynamic.m = vepc_data;
-    dynamic.locs = locs;
-    
-    % get volume (vol) size
-    vol_repeats = find(locs == locs(1));
-    vol_size(3) = vol_repeats(2)-vol_repeats(1);
-    [vol_size(1),vol_size(2),~] = size(vepc_data);
-    dynamic.vol_size = vol_size;
+% assign direction
+direction = info.Private_0051_1014;
+if contains(direction,'through')
+    dynamic.vz = vepc_data;
+elseif contains(direction,'ap')
+    dynamic.vx = vepc_data;
+elseif contains(direction,'fh')
+    dynamic.vy = vepc_data;
 end
-%% select roi
-% figure
-% montage(dynamic.m(:,:,1:24),colormap('gray'),'ThumbnailSize',[],'Size',[3 8]) % view slices
+%% Import magnitude data
+% go to desired magnitude image data directory
+dcm_files = dir('*IMA');
 
+% import magnitude data
+for k = 24:-1:1%length(dcm_files):-1:1
+    info = dicominfo(dcm_files(k).name);
+    vepc_data(:,:,k) = dicomread(info);
+    locs(k,1) = info.SliceLocation;
+end
 
-foi = 14; %frame of interest
+dynamic.m = vepc_data;
+dynamic.locs = locs;
 
-% SHOULD THIS BE SLICE OF INTEREST?
+% get volume (vol) size
+vol_repeats = find(locs == locs(1));
+vol_size(3) = vol_repeats(2) - vol_repeats(1);
+[vol_size(1),vol_size(2),~] = size(vepc_data);
+dynamic.vol_size = vol_size;
 
+%% Select slice to draw ROI
+montage(dynamic.m(:,:,1:24),colormap('gray'),'ThumbnailSize',[],'Size',[3 8]) % view slices
+slice = 14; % set slice of interest that ROI will be drawn on
+
+%% Draw ROI
 % matrix info
 m = dynamic.vol_size(1);
 n = dynamic.vol_size(2);
@@ -48,26 +54,25 @@ frs = length(dynamic.m)/h;
 
 % get roi and mask
 figure
-imshow(dynamic.m(:,:,foi),[])
+imshow(dynamic.m(:,:,slice),[])
 polygon = drawpolygon();
 roi = polygon.Position;
 roi = [roi;roi(1,:)]; %close roi by including first point again
 mask = poly2mask(roi(:,1), roi(:,2), m, n);
 close
 
-%% get average velocity for each frame
+%% Get average velocity for each frame
 v = zeros(1,frs);
-data = dynamic.vz(:,:,foi:h:h*frs);
+data = dynamic.vz(:,:,slice:h:h*frs);
 for fr = 1:frs
     im = squeeze(data(:,:,fr));
     im = im(mask);
     v(fr) = mean(im);
 end
 
+%% Plot results
 plot(v)
-%title('vz')
+title('vz')
 ylabel('velocity')
 xlabel ('frame')
 xlim([0 50])
-
-
